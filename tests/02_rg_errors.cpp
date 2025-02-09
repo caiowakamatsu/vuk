349dcb35246a7d4ec4ac67abbc2646bd26e0d6ec
Marcell Kiss <marcellk.kiss@gmail.com>
2025-02-09 12:46:59 +0000

    WIP

--- tests/02_rg_errors.cpp
+++ tests/02_rg_errors.cpp
@@ -17,38 +17,20 @@ using namespace vuk;
 TEST_CASE("error: can't construct incomplete") {
 	auto data = { 1u, 2u, 3u };
 	auto [b0, buf0] = create_buffer(*test_context.allocator, MemoryUsage::eGPUonly, DomainFlagBits::eAny, std::span(data));
-	auto buf1 = declare_buf("b1");
-	buf1->memory_usage = MemoryUsage::eGPUonly;
+	auto buf1 = declare_buf<uint32_t>("b1", { .memory_usage = MemoryUsage::eGPUonly });
 	buf1.same_size(buf0);
-	auto buf2 = declare_buf("b2");
-	buf2->memory_usage = MemoryUsage::eGPUonly;
+	auto buf2 = declare_buf<uint32_t>("b2", { .memory_usage = MemoryUsage::eGPUonly });
 	buf2.same_size(buf1);
-	auto buf3 = declare_buf("b3");
-	buf3->memory_usage = MemoryUsage::eGPUonly;
+	auto buf3 = declare_buf<uint32_t>("b3", { .memory_usage = MemoryUsage::eGPUonly });
 
-	auto copy = make_pass("cpy", [](CommandBuffer& cbuf, VUK_BA(Access::eTransferWrite) src, VUK_BA(Access::eTransferWrite) dst) {
-		cbuf.copy_buffer(src, dst);
+	auto copy =
+	    make_pass("cpy", [](CommandBuffer& cbuf, VUK_ARG(Buffer<uint32_t>, Access::eTransferWrite) src, VUK_ARG(Buffer<uint32_t>, Access::eTransferWrite) dst) {
+		    cbuf.copy_buffer(src->to_byte_view(), dst->to_byte_view());
 		return dst;
 	});
 
 	REQUIRE_THROWS(download_buffer(copy(std::move(buf0), std::move(buf3))).get(*test_context.allocator, test_context.compiler));
 }
-
-auto image2buf = make_pass("copy image to buffer", [](CommandBuffer& cbuf, VUK_IA(Access::eTransferRead) src, VUK_BA(Access::eTransferWrite) dst) {
-	BufferImageCopy bc;
-	bc.imageOffset = { 0, 0, 0 };
-	bc.bufferRowLength = 0;
-	bc.bufferImageHeight = 0;
-	bc.imageExtent = src->base_mip_extent();
-	bc.imageSubresource.aspectMask = format_to_aspect(src->format);
-	bc.imageSubresource.mipLevel = src->base_level;
-	bc.imageSubresource.baseArrayLayer = src->base_layer;
-	assert(src->layer_count == 1); // unsupported yet
-	bc.imageSubresource.layerCount = src->layer_count;
-	bc.bufferOffset = dst->offset;
-	cbuf.copy_image_to_buffer(src, dst, bc);
-	return dst;
-});
 
 auto blit_down = make_pass("blit down", [](CommandBuffer& cbuf, VUK_IA(Access::eTransferRead | Access::eTransferWrite) img) {
 	ImageBlit region = {};
@@ -73,7 +55,7 @@ auto blit_down = make_pass("blit down", [](CommandBuffer& cbuf, VUK_IA(Access::e
 
 TEST_CASE("error: read without write") {
 	{
-		auto dst = *allocate_buffer(*test_context.allocator, BufferCreateInfo{ MemoryUsage::eCPUonly, 100, 1 });
+		auto dst = *allocate_memory(*test_context.allocator, BufferCreateInfo{ MemoryUsage::eCPUonly, 100, 1 });
 		auto buf = vuk::discard_buf("a", *dst);
 
 		auto rd_buf = vuk::make_pass("rd", [](CommandBuffer&, VUK_BA(vuk::eTransferRead) buf) { return buf; });
@@ -85,7 +67,7 @@ TEST_CASE("error: read without write") {
 
 TEST_CASE("error: attaching something twice decl/decl") {
 	{
-		auto dst = *allocate_buffer(*test_context.allocator, BufferCreateInfo{ MemoryUsage::eCPUonly, 100, 1 });
+		auto dst = *allocate_memory(*test_context.allocator, BufferCreateInfo{ MemoryUsage::eCPUonly, 100, 1 });
 		auto buf_a = vuk::discard_buf("a", *dst);
 		auto buf_b = vuk::discard_buf("a again", *dst);
 
@@ -97,7 +79,7 @@ TEST_CASE("error: attaching something twice decl/decl") {
 /*
 TEST_CASE("not an error: attaching something twice acq/acq") {
 	{
-		auto dst = *allocate_buffer(*test_context.allocator, BufferCreateInfo{ MemoryUsage::eCPUonly, 100, 1 });
+		auto dst = *allocate_memory(*test_context.allocator, BufferCreateInfo{ MemoryUsage::eCPUonly, 100, 1 });
 		auto buf_a = vuk::acquire_buf("a", *dst, vuk::Access::eNone);
 		auto buf_b = vuk::acquire_buf("a again", *dst, vuk::Access::eNone);
 
@@ -109,7 +91,7 @@ TEST_CASE("not an error: attaching something twice acq/acq") {
 
 TEST_CASE("error: attaching something twice decl/acq") {
 	{
-		auto dst = *allocate_buffer(*test_context.allocator, BufferCreateInfo{ MemoryUsage::eCPUonly, 100, 1 });
+		auto dst = *allocate_memory(*test_context.allocator, BufferCreateInfo{ MemoryUsage::eCPUonly, 100, 1 });
 		auto buf_a = vuk::discard_buf("a", *dst);
 		auto buf_b = vuk::acquire_buf("a again", *dst, vuk::Access::eNone);
 
